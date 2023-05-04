79b56cf991961fdbdc8d25c1a4d86274d7a7f374
ardhanaawahyu <ardhanaawahyu@bee.id>
2023-05-04 19:29:20 +0700

    method/verb request and routing

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const Hapi = require("@hapi/hapi");
+const routes = require("./routes");
 
 const init = async () => {
   const server = Hapi.server({
@@ -6,8 +7,17 @@ const init = async () => {
     host: "localhost",
   });
 
+  server.route(routes);
+
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
 init();
+
+// how to start
+// npm run start
+// curl -X GET http://localhost:5000
+// curl -X GET http://localhost:5000/about
+// curl -X GET http://localhost:5000/any
+// curl -X POST http://localhost:5000
